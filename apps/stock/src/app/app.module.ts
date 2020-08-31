95fff15a3d3050093a1cc3583ded76b796274adb
aquileslh <aquiles.lazaro@circulodecredito.com.mx>
2020-08-30 23:10:39 -0500

    Se agregan complementos a vscode y creación de librería

--- apps/stock/src/app/app.module.ts
+++ apps/stock/src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { UiModule } from '@grillo-software/ui'
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    UiModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
