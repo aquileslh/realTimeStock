c3101968855effdb176f654983c5ecd1d9662d81
aquileslh <aquiles.lazaro@circulodecredito.com.mx>
2020-11-09 17:05:57 -0600

    dispara uno a uno los valores del array de los stock

--- libs/stock-list/src/lib/list/list.component.ts
+++ libs/stock-list/src/lib/list/list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { from, interval, of, timer } from 'rxjs';
 import { ForexSymbolService } from '../data-access/forex-symbol.service';
+import { concatMap, delay, map, mergeMap, take, timeout } from 'rxjs/operators';
 
 @Component({
   selector: 'grillo-software-list',
@@ -13,17 +15,28 @@ export class ListComponent implements OnInit {
 
   ngOnInit(): void {
     this.getSymbols('MX');
+    // this.emiteValue();
   }
 
   getSymbols(symbol: string): void {
     this.forexSymbol.forexSymbol(symbol).subscribe(
       (answers: any) => {
         this.forexSymbols = answers;
-        console.log(this.forexSymbol);
+        this.emiteValue(this.forexSymbols);
       },
       (error: any) => {
         console.log(error);
       }
     );
+  }
+
+  emiteValue(forexymbol: any) {
+    const emt = from(forexymbol);
+
+    const qwe = emt.pipe(concatMap((x) => of(x).pipe(delay(2000))));
+
+    qwe.subscribe((x: any) => {
+      console.log(x);
+    });
   }
 }
